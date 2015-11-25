a78853314f9096c8b39e98aff34a6c19e126df4b
Godai Azuma <godai0519@gmail.com>
2015-11-26 00:47:39 +0900

    Change SampleBuilder to be able to set a sample number.
    Use a command line argument '--num'
    Signed-off-by: Godai Azuma <godai0519@gmail.com>

--- Experiment/SampleBuilder/main.cpp
+++ Experiment/SampleBuilder/main.cpp
@@ -12,13 +12,20 @@
 #include <bayesian/serializer/bif.hpp>
 #include <bayesian/inference/likelihood_weighting.hpp>
 
-std::pair<std::string, std::string> process_command_line(int argc, char* argv[])
+struct command_line_t {
+    std::string const output;
+    std::string const network;
+    std::size_t const sample_size;
+};
+
+command_line_t process_command_line(int argc, char* argv[])
 {
     boost::program_options::options_description opt("Option");
     opt.add_options()
         ("help,h",                                                  "Show this help")
-        ("network,n", boost::program_options::value<std::string>(), "Network Path")
-        ("sample,s",  boost::program_options::value<std::string>(), "Sample Path");
+        ("output,o",  boost::program_options::value<std::string>(), "Sample Output Path    [required]")
+        ("network,n", boost::program_options::value<std::string>(), "Network Path          [required]")
+        ("num,i",     boost::program_options::value<std::string>(), "Generating Sample Num [required]");
 
 	boost::program_options::variables_map vm;
 	store(parse_command_line(argc, argv, opt), vm);
@@ -30,14 +37,16 @@ std::pair<std::string, std::string> process_command_line(int argc, char* argv[])
         std::exit(0);
     }
 
-    if(!vm.count("network") || !vm.count("sample"))
+    if(!vm.count("output") || !vm.count("network") || !vm.count("num"))
     {
-        std::cout << "Need --network and --sample" << std::endl;
+        std::cout << "Required: --output, --network and --num" << std::endl;
         std::cout << opt << std::endl;
         std::exit(0);
     }
 
-    return std::make_pair(vm["network"].as<std::string>(), vm["sample"].as<std::string>());
+    std::size_t const num = std::stoull(vm["num"].as<std::string>());
+
+    return { vm["output"].as<std::string>(), vm["network"].as<std::string>(), num };
 }
 
 template<class OutputStream>
@@ -62,12 +71,10 @@ void write_sample(OutputStream& ost, std::vector<bn::vertex_type> const& vertex_
 int main(int argc, char* argv[])
 {
     // コマンドラインパース
-    std::string graph_path;
-    std::string sample_path;
-    std::tie(graph_path, sample_path) = process_command_line(argc, argv);
+    auto const command_line = process_command_line(argc, argv);
 
     // グラフファイルを開いてgraph_dataに導入
-    std::ifstream ifs(graph_path);
+    std::ifstream ifs(command_line.network);
     std::string const graph_data{std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>()};
     ifs.close();
     std::cout << "Loaded Graph: Length = " << graph_data.size() << std::endl;
@@ -79,10 +86,10 @@ int main(int argc, char* argv[])
 
     // サンプルのmake
     bn::inference::likelihood_weighting lw(graph);
-    auto const samples = lw.make_samples({}, 2000000);
+    auto const samples = lw.make_samples({}, command_line.sample_size);
 
     // サンプルの書出
-    std::ofstream ofs(sample_path);
+    std::ofstream ofs(command_line.output);
     write_sample(ofs, vertex_list, samples);
     ofs.close();
 }
