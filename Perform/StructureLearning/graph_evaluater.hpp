d1198cfe1872f1c9f7616d3f67c67fd41cbcbc8b
Godai Azuma <godai0519@gmail.com>
2015-11-26 00:08:24 +0900

    Remove Perform/* because it isn't managed for a long time.
    Signed-off-by: Godai Azuma <godai0519@gmail.com>

--- Perform/StructureLearning/graph_evaluater.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PRE_EXP_GRAPH_EVALUATER_HPP
-#define PRE_EXP_GRAPH_EVALUATER_HPP
-
-#include <bayesian/graph.hpp>
-
-std::size_t count_disappeared_link(bn::graph_t const& teacher, bn::graph_t const& target);
-std::size_t count_appeared_link(bn::graph_t const& teacher, bn::graph_t const& target);
-
-#endif
